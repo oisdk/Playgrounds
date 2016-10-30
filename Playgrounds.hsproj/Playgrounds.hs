97ebd210896359fa1da914352386176033ba79f5
oisdk <oisin.kidney@gmail.com>
2016-10-30 13:19:50 +0000

    patricia

--- Playgrounds.hsproj/Playgrounds.hs
+++ Playgrounds.hsproj/Playgrounds.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE DeriveFunctor #-}
+{-# Language LambdaCase #-}
 
 import Data.Map.Strict (Map, lookup, alter, fromList, singleton)
+import Test.QuickCheck
 import qualified Data.Map.Strict as Map
 import Data.List (stripPrefix)
 import Prelude hiding (lookup)
 import Data.Maybe
-import Test.QuickCheck
+import Data.Foldable
 
 data SplitResult a = SplitResult
   { common :: [a]
@@ -13,11 +15,12 @@ data SplitResult a = SplitResult
   , rTail  :: [a] 
   } deriving Show
 
-both :: ([a] -> [a]) -> SplitResult a -> SplitResult a
-both f s = SplitResult (f (common s)) (lTail s) (rTail s)
+prepCommon :: a -> SplitResult a -> SplitResult a
+prepCommon x ~(SplitResult xs ys zs) = SplitResult (x:xs) ys zs
 
 followSplit :: Eq a => [a] -> [a] -> SplitResult a
-followSplit (x:xs) (y:ys) | x == y = both (x:) (followSplit xs ys)
+followSplit (x:xs) (y:ys)
+  | x == y = prepCommon x (followSplit xs ys)
 followSplit xs ys = SplitResult [] xs ys
 
 data Trie a = Trie
@@ -28,8 +31,7 @@ data Trie a = Trie
 member :: Ord a => [a] -> Trie a -> Bool
 member [] t = endsHere t
 member (x:xs) (Trie _ c) = maybe False f (lookup x c) where
-  f (ys,t) = maybe False (flip member t) (stripPrefix ys xs)
-
+  f (ys,t) = maybe False (flip member t) (stripPrefix (toList ys) xs)
 
 emptyT :: Trie a
 emptyT = Trie False Map.empty
@@ -38,16 +40,19 @@ insert :: Ord a => [a] -> Trie a -> Trie a
 insert [] (Trie _ c) = Trie True c
 insert (x:xs) (Trie e c) = Trie e (alter f x c) where
   f Nothing = Just (xs,Trie True Map.empty)
-  f (Just (ys,t)) = case followSplit xs ys of
-    SplitResult cs xs [] -> Just (cs, insert xs t)
-    SplitResult cs [] (y:ys) -> Just (cs, Trie True (singleton y (ys,t)))
-    SplitResult cs (x:xs) (y:ys) -> Just (cs, Trie False (fromList   [(x,(xs,Trie True Map.empty)), (y,(ys,t))]))
-    
-instance (Arbitrary a, Ord a) => Arbitrary (Trie a) where
-  arbitrary = (foldr insert emptyT :: Ord a => [[a]] -> Trie a) <$> arbitrary 
-  
-insertIsIn :: Ord a => Trie a -> [a] -> Bool
-insertIsIn t xs = member xs (insert xs t)
+  f (Just (ys,t)) = let (SplitResult cs xxs yys) = followSplit xs ys in Just (cs, g xxs yys) where
+    g xs [] = insert xs t
+    g [] (y:ys) =  Trie True (singleton y (ys,t))
+    g (x:xs) (y:ys) = Trie False (fromList [(x,(xs,Trie True Map.empty)), (y,(ys,t))])
 
-sameOrder :: Ord a => [[a]] -> Bool
-sameOrder xs = foldr insert emptyT xs == foldr insert emptyT (reverse xs)
+fromListT :: Ord a => [[a]] -> Trie a
+fromListT = foldr insert emptyT
+
+insertIsMember :: Trie Int -> [Int] -> Bool
+insertIsMember t xs = member xs (insert xs t)
+
+orderDoesntMatter :: [[Int]] -> Bool
+orderDoesntMatter xs = fromListT xs == fromListT (reverse xs)
+
+instance (Arbitrary a, Ord a) => Arbitrary (Trie a) where
+  arbitrary = fromListT <$> arbitrary
