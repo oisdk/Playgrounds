791124f05de8321fa1a89d53e677a9e5719a9025
Donnacha Oisín Kidney <mail@doisinkidney.com>
2016-11-16 23:31:32 +0000

    close

--- Playgrounds.hsproj/Playgrounds.hs
+++ Playgrounds.hsproj/Playgrounds.hs
@@ -38,66 +38,41 @@ trips :: ( MonadPlus m
       => m (Integer,Integer,Integer)
 trips = [ (x,y,z) | x <- [1..], y <- [1..], z <- [1..], x*x + y*y == z*z ]
 
-instance (Semiring r, Applicative m) => Semiring (ContT r m a) where
-  one  = ContT (const (pure one))
-  zero = ContT (const (pure one))
-  f <+> g = ContT (\k -> liftA2 (<+>) (runContT f k) (runContT g k))
-  f <.> g = ContT (\k -> liftA2 (<.>) (runContT f k) (runContT g k))
-  
-instance Semiring a => Semiring [a] where
-  one = [one]
-  zero = []
-  [] <+> ys = ys
-  xs <+> [] = xs
-  (x:xs) <+> (y:ys) = (x <+> y) : (xs <+> ys)
-  [] <.> _ = []
-  _ <.> [] = []
-  (x:xs) <.> (y:ys) =
-    (x <.> y) : (map (x <.>) ys <+> map (<.> y) xs <+> (xs <.> ys))
-
 instance (Monoid r, Applicative m) => Monoid (ContT r m a) where
   mempty = ContT (const (pure mempty))
   mappend (ContT f) (ContT g) = ContT (\x -> liftA2 mappend (f x) (g x))
- 
-
-newtype Bag a = Bag { getBag :: [a] } deriving (Functor, Applicative, Monad, Monoid, Foldable)
-
-instance Monoid a => Semiring  (Bag a) where
-  zero = mempty
-  (<+>) = mappend
-  one = pure mempty
-  (<.>) = liftA2 mappend
-
-
-
 
 newtype List a = List { runList :: forall m. Monoid m => Cont m a } deriving Functor
-newtype LList a = LList { runLList :: forall m. Semiring m => Cont m a } deriving Functor
-
-instance Semiring (LList a) where
-  zero = LList zero
-  one = LList one
-  LList x <+> LList y = LList (x <+> y)
-  LList x <.> LList y = LList (x <.> y)
-
-instance Foldable LList where
-  foldMap f (LList x) = fold (runCont x (Bag . pure . f))
 
 instance Foldable List where
   foldMap = flip (runCont.runList)
+  
+instance Show a => Show (List a) where
+  show = show . foldr (:) []
+  
 
 instance Monoid (List a) where
   mappend (List x) (List y) = List (mappend x y)
   mempty = List mempty
+  
+instance Monoid a => Semiring (List a) where
+  zero = mempty
+  (<+>) = mappend
+  (<.>) = liftA2 mappend
+  one = pure mempty
 
 bfs :: List a -> [a]
-bfs a = (toList . fold . levels) (foldMap yield a) where yield x = Levels [pure x]
+bfs a = (toList . fold . levels) (anyOf a) where yield x = Levels [pure x]
 
-newtype Levels a = Levels { levels :: [List a] }
+newtype Levels a = Levels { levels :: [List a] } deriving Functor
 
-instance Monoid (Levels a) where
-  mempty        = Levels []
-  x `mappend` y = Levels (mempty : merge (levels x) (levels y))
+instance Applicative Levels where
+  pure x = Levels [pure x]
+  Levels fs <*> Levels xs = Levels [ f <*> x | f <- fs, x <- xs ]
+  
+instance Alternative Levels where
+  empty = Levels []
+  Levels x <|> Levels y = Levels (mempty : merge x y)
 
 instance IsList (List a) where
   type Item (List a) = a
@@ -108,34 +83,19 @@ instance Applicative List where
   pure x = List (pure x)
   (<*>) = ap
 
-instance Applicative LList where
-  pure x = LList (pure x)
-  (<*>) = ap
-
 instance Alternative List where
   empty = mempty
   (<|>) = mappend
-  
-instance Alternative LList where
-  empty = mzero
-  (<|>) = mplus
 
 instance Monad List where
   x >>= f = foldMap f x
-
-instance Monad LList where
-  x >>= f = add (fmap f x)
 
 instance MonadPlus List where
   mzero = mempty
   mplus = mappend
 
-instance MonadPlus LList where
-  mzero = zero
-  x `mplus` y = x <+> y
-
-anyOf :: Alternative m => [a] -> m a
-anyOf = foldr ((<|>) . pure) empty 
+anyOf :: (Alternative m, Foldable f) => f a -> m a
+anyOf = getAlt . foldMap (Alt . pure)
 
 -- like 'zipWith append' without cutting the longer list
 merge :: [List a] -> [List a] -> [List a]
