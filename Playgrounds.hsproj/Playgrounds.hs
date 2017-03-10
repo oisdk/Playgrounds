811e450624a604cef71f03cd27b1e8386d15872a
Donnacha Oisín Kidney <mail@doisinkidney.com>
2017-03-10 18:39:33 +0000

    Probs

--- Playgrounds.hsproj/Playgrounds.hs
+++ Playgrounds.hsproj/Playgrounds.hs
@@ -3,91 +3,88 @@
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import Data.Ratio
-import Prelude hiding (Either(..)) -- hiding (Functor(..), Applicative(..), (<$>))
+import Prelude hiding (Functor(..), Applicative(..), (<$>))
 import qualified Prelude
 import GHC.Exts
 
-newtype Prob s a = Prob { runProb :: [(a,s)] } deriving Prelude.Functor
 
-class Functor f where
---  type Suitable f a :: Constraint
---  fmap :: Suitable f b => (a -> b) -> f a -> f b
---  
---class Functor f => Applicative f where
---  lower :: Suitable f a => Free f a -> f a
-
-instance Num s => Prelude.Applicative (Prob s) where
-  pure x = Prob [(x,1)]
-  Prob fs <*> Prob xs
-    = Prob
-    [ (f x, fp * xp)
-    | (f,fp) <- fs
-    , (x,xp) <- xs]
-
-newtype Reified s a = Reified { runReified :: Map a s }
-
-data Free f xs a where
-  Pure :: a -> Free 'Empty f a
-  Ap :: Free xs f (b -> a) -> f ys b -> Free (Node ys b xs) f a
+data Prob a where
+  Prob :: (Enum a, Bounded a) => (a -> Double) -> Prob a
   
-data Tree a = Empty | Node (Tree a) a (Tree a)
+toDist :: Prob a -> Dist a
+toDist (Prob f) = Dist [ (x, f x) | x <- [minBound..maxBound] ]
 
-data Memo f xs a
-  = Memo
-  { run :: f a
-  , cmp :: Free xs (Memo f) a }
-  
-data Elem (x :: k) (xs :: Tree k) :: * where
-  Here :: Elem x (Node xs x ys)
-  Left :: Elem x xs -> Elem x (Node xs y ys)
-  Right :: Elem x ys -> Elem x (Node xs y ys)
-  
-back :: Num s => (forall a. f a -> (a -> s)) -> Elem x xs -> Memo f xs a -> f x
-back t prf (Memo r c) = go t prf c r where
-  go :: (forall a. f a -> (a -> s)) -> Elem x xs -> Free xs (Memo f) a -> f a -> f x
-  go t Here (Ap _ xs) _ = run xs
-  go t (Left prf) (Ap _ xs) _ = back t prf xs
---  go (Right prf) (Ap f _) _ = 
-    
---instance Functor f => Functor (Memo f) where
---  fmap f (Memo r c) = 
 
---instance Prelude.Functor (Free f) where
---  fmap f (Pure a) = Pure (f a)
---  fmap f (Ap x y) = Ap ((f .) Prelude.<$> x) y
+  
+newtype Dist a = Dist {runDist :: [(a, Double)]} deriving Prelude.Functor
+
+instance Prelude.Applicative Dist where
+  pure x = Dist [(x,1)]
+  Dist fs <*> Dist xs = Dist [(f x, fp*xp) | (f,fp) <- fs, (x,xp) <- xs ]
+
+  
+tot :: Prob a -> Double
+tot (Prob f) = sum (map f [minBound..maxBound])
+
+weight :: Prob a -> Dist a -> Dist a
+weight (Prob f) (Dist xs) = Dist [(x, xp * f x) | (x,xp) <- xs ]
+
+weightP :: (a -> Double) -> Prob a -> Prob a
+weightP f (Prob x) = intersect (Prob f) (Prob x)
+  
+intersect :: Prob a -> Prob a -> Prob a
+intersect (Prob xs) (Prob ys) = Prob (\x -> xs x * ys x)
+
+data Free xs f a where
+  Pure :: a -> Free '[] f a
+  Ap :: Free xs f (b -> a) -> f b -> Free (b ': xs) f a
+
+lift :: f a -> Free '[a] f a
+lift = Ap (Pure id)
+
+type family Func (xs :: [*]) (y :: *) :: * where
+  Func '[] y = y
+  Func (x ': xs) y = Func xs (x -> y)
+
+data Elem (x :: k) (xs :: [k]) where
+  Here :: Elem x (x ': xs)
+  There :: Elem x xs -> Elem x (y ': xs)
+  
+(<$$>) :: a -> Free xs f (a -> b) -> Free xs f b
+(<$$>) x = fmap ($x)
+
+(<$>) = fmap
+
+back :: Elem x xs -> Prob a -> Free xs Prob a -> Prob x
+back Here r (Ap f x) = weightP (\b -> (sum . map snd . runDist) $ weight r (infer toDist (b <$$> f))) x
+
+fmap :: (a -> b) -> Free xs f a -> Free xs f b
+fmap f (Pure a) = Pure (f a)
+fmap f (Ap x y) = Ap (fmap (f .) x) y
 --
---instance Prelude.Applicative (Free f) where
---  pure = Pure
---  Pure f <*> y = Prelude.fmap f y
---  Ap x y <*> z = Ap (flip Prelude.<$> x Prelude.<*> z) y
 --
---data Graph f a
---  = Graph
---  { comp :: Free (Graph f) a
---  , infer :: f a }
---
---instance Functor f => Functor (Graph f) where
---  type Suitable (Graph f) a = Suitable f a
---  fmap f g = Graph (fmap f (comp
---
+pure :: a -> Free '[] f a
+pure = Pure
+
+(<*>) :: Free xs f (a -> b) -> Free ys f a -> Free (xs ++ ys) f b
+Pure f <*> y = fmap f y
+Ap x y <*> z = Ap (flip <$> x <*> z) y
+
 --reify :: (Num s, Ord a) => Prob s a -> Reified s a
 --reify = Reified . Map.fromListWith (+) . runProb
 --
 --reflect :: Reified s a -> Prob s a
 --reflect = Prob . Map.toList . runReified
---
---runAp :: Prelude.Applicative g => (forall x. f x -> g x) -> Free f a -> g a
---runAp u (Pure x) = Prelude.pure x
---runAp u (Ap f x) = runAp u f Prelude.<*> u x
---
---runApC :: (forall x. f x -> g x) -> Free f a -> Free g a
---runApC u (Pure x) = Pure x
---runApC u (Ap f x) = Ap (runApC u f) (u x)
---
---
---runInfer :: Ord a => Free (Reified Rational) a -> Reified Rational a
---runInfer = reify . runAp reflect
 
+infer :: Prelude.Applicative g => (f ~> g) -> Free xs f ~> g
+infer _ (Pure x) = Prelude.pure x
+infer u (Ap x y) = infer u x Prelude.<*> u y
+
+type f ~> g = forall a. f a -> g a
+
+hoist :: (f ~> g) -> Free xs f ~> Free xs g
+hoist _ (Pure a) = Pure a
+hoist f (Ap y x) = Ap (hoist f y) (f x) 
 
 --die :: FactorGraph (Reified Rational) Integer
 --die 
@@ -98,4 +95,6 @@ back t prf (Memo r c) = go t prf c r where
 --lift :: f a -> Free f a
 --lift = Ap (Pure id)
 --
-
+type family (++) (xs :: [k]) (ys :: [k]) :: [k] where
+  '[] ++ ys = ys
+  (x ': xs) ++ ys = x ': (xs ++ ys)
